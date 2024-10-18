4132963297ab5877bb59497774c169345690092b
MuhammadUsman7l <usmanjaleel223@gmail.com>
2024-10-18 08:43:09 +0500

    Creating Components

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import ProductCard from "./ProductCard";
+import ShoppingCart from "./ShoppingCart";
+
+export { Header, Footer, ProductCard, ShoppingCart };
